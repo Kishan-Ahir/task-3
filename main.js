5f5d0f32a3a1d068815a2f3449c4d0102bf9be77
Kishan-Ahir <chandravadiyakishan77@gmail.com>
2023-01-30 09:25:50 +0530

    get element by class name

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 //let title=document.getElementById('main-header');
 //title.style.border ="red 3px solid";
 //title.style.borderRadius ="30px";
-let additems=document.all[14];
-additems.style.color = "green";
-additems.style.fontWeight  ="bold";
+//let additems=document.all[14];
+//additems.style.color = "green";
+//additems.style.fontWeight  ="bold";
+let items = document.getElementsByClassName('list-group-item');
+console.log(items);
+
+items[1].style.backgroundColor = "lightpink";
+
+//Will give an error
+//items.style.backgroundColor = "red";
+
+for(let i=0;i<items.length;i++)
+{
+    items[i].style.backgroundColor = "lightyellow";
+};
