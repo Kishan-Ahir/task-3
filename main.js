30463bbc27eefe9a679f30622fe29a35ca7876e9
Kishan-Ahir <chandravadiyakishan77@gmail.com>
2023-01-30 08:32:14 +0530

    javascript for form

---
+++ main.js
@@ -0,0 +1,6 @@
+//let title=document.getElementById('main-header');
+//title.style.border ="red 3px solid";
+//title.style.borderRadius ="30px";
+let additems=document.all[14];
+additems.style.color = "green";
+additems.style.fontWeight  ="bold";
