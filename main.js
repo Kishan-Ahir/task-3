564cb42134ad8971fd7bd7c0681e629fbfe49335
Kishan-Ahir <chandravadiyakishan77@gmail.com>
2023-01-30 10:51:30 +0530

    querry selector all

--- main.js
+++ main.js
@@ -44,11 +44,23 @@
 
 // QUERRY SELECTOR
 
-let item = document.querySelector(".list-group-item");
-item.style.color = "red";
+//let item = document.querySelector(".list-group-item");
+//item.style.color = "red";
 
-item2 = document.querySelector(".list-group-item:nth-child(2)");
-item2.style.color = "green";
+//item2 = document.querySelector(".list-group-item:nth-child(2)");
+//item2.style.color = "green";
 
-let item3 = document.querySelector(".list-group-item:nth-child(3)")
-item3.style.display = "none";
+//let item3 = document.querySelector(".list-group-item:nth-child(3)")
+//item3.style.display = "none";
+
+//  QUERRY SELECTOR  ALL  
+
+let items = document.querySelectorAll(".list-group-item");
+items[1].style.color = "green";
+
+let odd=document.querySelectorAll(".list-group-item:nth-child(odd)");
+
+for(let i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor = "green";
+}
